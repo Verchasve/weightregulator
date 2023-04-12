a8c30d44f26a1cd955213569d395917616b82136
Deepak Singh Rathore <rathore270482@gmail.com>
2023-04-12 14:01:04 +0530

    12-04-2022

--- src/component/SetBrandList.js
+++ src/component/SetBrandList.js
@@ -1,60 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../App.css'
 
-
-function SetBrandList() {
+function SetBrandList () {
   const navigate = useNavigate()
-  const [brands, setBrands] = useState([]);
-  const [newBrand, setNewBrand] = useState('');
+  const [brands, setBrands] = useState([])
+  const [newBrand, setNewBrand] = useState('')
 
-  const handleInputChange = (event) => {
-    setNewBrand(event.target.value);
-  };
+  const handleInputChange = event => {
+    setNewBrand(event.target.value)
+  }
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
+  const handleFormSubmit = event => {
+    event.preventDefault()
     if (newBrand.trim() === '') {
-      return;
+      return
     }
-    setBrands([...brands, { id: Date.now(), text: newBrand }]);
-    setNewBrand('');
-  };
+    setBrands([...brands, { id: Date.now(), text: newBrand }])
+    setNewBrand('')
+  }
 
-  const handleBrandDelete = (id) => {
-    setBrands(brands.filter((brand) => brand.id !== id));
-  };
+  const handleBrandDelete = id => {
+    setBrands(brands.filter(brand => brand.id !== id))
+  }
 
-  let brandStyle={
-    border: "2px red"
+  let brandStyle = {
+    border: '2px red'
   }
 
   return (
     <div>
-      
       <form className='container my-3' onSubmit={handleFormSubmit}>
-      <label htmlFor="title" className="form-label mx-3"><strong> Set Brand </strong></label>
-        <input type="text" value={newBrand} onChange={handleInputChange} />
-        <button className='mx-2'  type="submit">Add</button>
+        <label htmlFor='title' className='form-label mx-3'>
+          <strong> Set Brand </strong>
+        </label>
+        <input type='text' value={newBrand} onChange={handleInputChange} />
+        <button className='mx-2' type='submit'>
+          Add
+        </button>
         <button onClick={() => navigate(-1)}> Back </button>
       </form>
-      
-      <div className='container' style={brandStyle}>
-      <ul >
-        {brands.map((brand) => (
-          <li key={brand.id}>
-            {brand.text}
-            <button className='btn btn-sm btn-danger my-2 mx-2' onClick={() => handleBrandDelete(brand.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-      <button className='mx-2'>Save</button>
+
+      <div className='container'>
+        <ul
+          style={{
+            verticalAlign: 'auto',
+            width: 'fit-content',
+            alignSelf: 'center',
+            alignItems: 'flex-end',
+            backgroundColor: 'beige',
+            borderColor: 'beige',
+            borderWidth: '1px',
+            borderStyle: 'solid',
+            borderRadius: '5%'
+          }}
+        >
+          {brands.map(brand => (
+            <li key={brand.id}>
+              {brand.text}
+              <button
+                className='btn btn-block btn-danger my-2 mx-2'
+                onClick={() => handleBrandDelete(brand.id)}
+              >
+                <span class='fa fa-trash'> </span> Delete{' '}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button className='mx-2'>Save</button>
       </div>
     </div>
-
-  );
+  )
 }
 
-export default SetBrandList;
-
-
+export default SetBrandList
