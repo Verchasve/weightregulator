21a01e277dd91383a1011425056eef95be4dfb33
Deepak Singh Rathore <rathore270482@gmail.com>
2023-04-26 19:23:43 +0530

    error resolved

--- src/component/SetOperatorList.js
+++ src/component/SetOperatorList.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import '../App.css'
+const Settings = require('../settings')
+
 
 const SetOperatorList = () => {
     const navigate = useNavigate()
